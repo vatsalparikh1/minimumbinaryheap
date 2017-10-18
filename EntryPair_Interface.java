6c243e3acc73b41ab3e8e937a860c66447550c4b
vatsalparikh1 <vatsal@email.unc.edu>
2017-10-18 17:09:54 -0400

    Add files via upload

---
+++ EntryPair_Interface.java
@@ -0,0 +1,7 @@
+package MinBinHeap_A3;
+
+public interface EntryPair_Interface {
+  String getValue();
+  int getPriority();
+}
+
---
+++ MinBinHeap.java
@@ -0,0 +1,157 @@
+package MinBinHeap_A3;
+
+public class MinBinHeap implements Heap_Interface {
+  private EntryPair[] array; //load this array
+  private int size;
+  private static final int arraySize = 10000; //Everything in the array will initially 
+                                              //be null. This is ok! Just build out 
+                                              //from array[1]
+
+  public MinBinHeap() {
+    this.array = new EntryPair[arraySize];
+    array[0] = new EntryPair(null, -100000); //0th will be unused for simplicity 
+                                             //of child/parent computations...
+                                             //the book/animation page both do this.
+  }
+    
+  //Please do not remove or modify this method! Used to test your entire Heap.
+  @Override
+  public EntryPair[] getHeap() { 
+    return this.array;
+  }
+  
+  
+  
+  
+  
+@Override
+public void insert(EntryPair entry) {
+	
+	
+	int lastIndex = size();
+	
+	//insert entry to lastIndex
+	array[lastIndex + 1] = entry;
+	size++;
+	
+	bubbleUp(size());
+	
+
+	
+		
+	
+	
+}
+
+@Override
+public void delMin() {
+	
+	if(size() <= 0){ // if there is nothing to delete
+		return;
+	}
+	
+	
+    array[1] = array[size()];
+    size--;
+    bubbleDown(1);
+
+	return;
+}
+
+@Override
+public EntryPair getMin() { //return min element (root)
+	
+	if(size() <= 0){
+		return null;
+	}
+	
+	return array[1];
+	
+}
+
+@Override
+public int size() {
+	
+	return size;
+}
+
+@Override
+public void build(EntryPair[] entries) {
+	
+	for(int i = 0; i < entries.length; i++){
+		array[i+1] = entries[i];
+	}
+	
+	size=entries.length;
+	
+	for( int i = size() / 2; i > 0; i-- ){
+        bubbleDown( i );
+	}
+	
+	
+	
+}
+
+
+//only call if there is a left child
+public void bubbleDown(int index){
+	int leftIndex = index*2;
+	int rightIndex = (index*2) + 1;
+	int smallIndex;
+	
+	if(rightIndex < size() + 1){//has a right child
+		if(array[leftIndex].priority < array[rightIndex].priority){
+			smallIndex = leftIndex;
+		}else{
+			smallIndex = rightIndex;
+		}
+		
+	}else{ //has only a left child
+		smallIndex = leftIndex;
+	}
+	
+	
+	if(array[smallIndex].priority < array[index].priority){//swap
+		EntryPair temp = array[smallIndex];
+		array[smallIndex] = array[index];
+		array[index] = temp;
+	}else{//index has higher priority than children (do nothing)
+		return;
+	}
+	
+	if((smallIndex*2) < size() + 1){//if swapped index is not leaf
+		bubbleDown(smallIndex);
+	}else{//swap index points to a leaf
+		return;
+	}
+	
+	
+}
+
+
+public void bubbleUp(int index){ //called on by insert
+	
+	int parentIndex = (int) Math.floor(index/2);
+	
+	if(array[index].priority < array[parentIndex].priority){//swap
+		EntryPair temp = array[index];
+		array[index] = array[parentIndex];
+		array[parentIndex] = temp;
+		
+		if((parentIndex !=1) && (parentIndex != 0)){ //if parentIndex has a parent
+			bubbleUp(parentIndex);
+		}else{
+			return;
+		}
+		
+	}else{ //do nothing (end)
+		return;
+	}
+	
+	
+}
+
+
+
+
+}
